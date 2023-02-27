2518e9f6ddecdaca8a4bcc06282f91212357e4b0
aylinair <aylinair25@gmail.com>
2023-02-27 07:10:15 +0700

    tes commit

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Destination;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\ApiCollection;
+use App\Models\UserRole;
+use Illuminate\Http\Request;
+
+class DestinationController extends Controller
+{
+    //
+    
+}
---
+++ database/migrations/2023_02_26_034932_create_destinations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('destinationName');
+            $table->string('destinationTag');
+            $table->string('destinationAddress');
+            $table->string('destinationImage');
+            $table->string('destinationLoc');
+            $table->string('destinationDecs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('destinations');
+    }
+};
