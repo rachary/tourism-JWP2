996cc23d0bc16a9901fe43308c16bf77acd0a07a
izraaliya <izra.aliya28@gmail.com>
2023-03-03 18:05:13 +0700

    pesan_commit

--- resources/js/views/app/HomeView.vue
+++ resources/js/views/app/HomeView.vue
@@ -36,7 +36,7 @@
 <script setup>
 
 
-import NavbarComponent from '../../components/NavbarComponent.vue';
+import NavbarComponent from '../components/NavbarComponent.vue';
 import Content1Component from '../../components/Content1Component.vue';
 import Content2Component from '../../components/Content2Component.vue';
 import Content3Component from '../../components/Content3Component.vue';
