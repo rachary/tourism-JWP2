c71397a63638f0343225f608369e2a6b90a4ad2a
rachary <rachmad.ary1@gmail.com>
2023-03-12 03:17:53 +0700

    route api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\DestinationController;
+use App\Http\Controllers\DestinationImageController;
+use App\Http\Controllers\DestinationRegionController;
+use App\Http\Controllers\DestinationTagController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserRoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,15 +21,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/user/role', [UserRoleController::class, 'index'])->name('role');
+Route::get('/destination/region', [DestinationRegionController::class, 'index'])->name('region');
+Route::get('/destination/tag', [DestinationTagController::class, 'index'])->name('tag');
+Route::get('/destination/image', [DestinationImageController::class, 'index'])->name('image');
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) 
-{
-    return $request->user();
-});
-
-Route::controller(UserController::class)->prefix('user')->group(function() {
+Route::middleware('auth:sanctum')->controller(UserController::class)->prefix('user')->group(function() {
     Route::get('', 'index')->name('user.index');
     Route::post('', 'store')->name('user.store');
     Route::get('{id?}', 'show')->name('user.show');
@@ -36,10 +39,12 @@ Route::controller(UserController::class)->prefix('user')->group(function() {
 Route::controller(DestinationController::class)->prefix('destination')->group(function() {
     Route::get('', 'index')->name('destination.index');
     Route::post('', 'store')->name('destination.store');
-    // Route::get('{id?}', 'show')->name('destination.show');
-    // Route::put('{id?}', 'update')->name('destination.update');
-    // Route::delete('{id?}', 'destroy')->name('destination.destroy');
+    Route::get('{id?}', 'show')->name('destination.show');
+    Route::put('{id?}', 'update')->name('destination.update');
+    Route::delete('{id?}', 'destroy')->name('destination.destroy');
 });
+
+
 
 Route::controller(EventController::class)->prefix('event')->group(function() {
     Route::get('', 'index')->name('event.index');
